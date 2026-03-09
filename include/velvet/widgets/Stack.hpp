14a743b3bdfef6655d5dda6b0445d2c13605e55f
Rushil Koul <ruskoul3@gmail.com>
2026-03-09 20:08:49 +0530

    refactored stack implementation. improved efficiency

---
+++ include/velvet/widgets/Stack.hpp
@@ -0,0 +1,142 @@
+#pragma once
+#include <velvet/base/Widget.hpp>
+#include <vector>
+
+enum class StackDirection { Horizontal, Vertical };
+
+class Stack : public Widget {
+private:
+    float xPos = 0;
+    float yPos = 0;
+
+    float nextPos = 0;
+    float cachedWidth = 0;
+    float cachedHeight = 0;
+
+    std::vector<Widget*> children;
+
+    StackDirection direction;
+
+    float gap;
+
+    bool dirty = false;
+
+    void draw(sf::RenderWindow& window) override {
+        for (Widget* child : children) {
+            child->render(window);
+        }
+
+    }
+
+    void update(sf::RenderWindow& window) override {
+        if (dirty) {
+            recalculateChildPositions();
+            dirty = false;
+        }
+
+    }
+
+    void recalculateChildPositions() {
+        float pos = (direction == StackDirection::Horizontal) ? xPos : yPos;
+
+        for (Widget* child : children) {
+            if (direction == StackDirection::Horizontal) {
+                child->setPosition(pos, yPos);
+                pos += child->getDimensions().x + gap;
+            } else {
+                child->setPosition(xPos, pos);
+                pos += child->getDimensions().y + gap;
+            }
+        }
+        nextPos = pos;
+    }
+
+public:
+    Stack(StackDirection dir, float gap = 0) : direction(dir), gap(gap), nextPos(0) {}
+
+    void render(sf::RenderWindow& window) override {
+        update(window);
+        draw(window);
+    }
+
+    void add(Widget* widget) {
+
+        sf::Vector2f dims = widget->getDimensions();
+
+        if (direction == StackDirection::Horizontal) {
+            widget->setPosition(xPos + nextPos, yPos);
+
+            nextPos += dims.x + gap;
+            cachedWidth = nextPos - gap;
+
+            if (dims.y > cachedHeight) cachedHeight = dims.y;
+        } else {
+            widget->setPosition(xPos, yPos + nextPos);
+
+            nextPos += dims.y + gap;
+            cachedHeight = nextPos - gap;
+
+            if (dims.x > cachedWidth) cachedWidth = dims.x;
+        }
+
+        children.push_back(widget);
+    }
+
+    void setPosition(float x, float y) override {
+        if (xPos != x || yPos != y) {
+            xPos = x;
+            yPos = y;
+            dirty = true;
+        }
+    }
+
+    sf::Vector2f getDimensions() override {
+        return sf::Vector2f(cachedWidth, cachedHeight);
+    }
+
+    void setGap(float g) {
+        if (gap != g) {
+            gap = g;
+            nextPos = 0;
+            cachedWidth = 0;
+            cachedHeight = 0;
+
+            for (Widget* child : children) {
+                sf::Vector2f dims = child->getDimensions();
+
+                if (direction == StackDirection::Horizontal) {
+                    nextPos += dims.x + gap;
+                    if (dims.y > cachedHeight) cachedHeight = dims.y;
+
+                } else {
+                    nextPos += dims.y + gap;
+                    if (dims.x > cachedWidth) cachedWidth = dims.x;
+
+                }
+
+            }
+            if (!children.empty()) {
+                if (direction == StackDirection::Horizontal)
+                    cachedWidth = nextPos - gap;
+                else
+                    cachedHeight = nextPos - gap;
+            }
+            dirty = true;
+        }
+    }
+
+    float getGap() const { return gap; }
+};
+
+// preserve HStack and VStack class names for backwards compatibility
+// (to be fair its like 4 commits ago but still cmon)
+
+class HStack : public Stack {
+public:
+    HStack(float gap = 0) : Stack(StackDirection::Horizontal, gap) {}
+};
+
+class VStack : public Stack {
+public:
+    VStack(float gap = 0) : Stack(StackDirection::Vertical, gap) {}
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,75 +1,16 @@
 #include <velvet/core>
 
-/*
-int main() {
-    Window window(800, 600, "Velvet");
-    //VStack vstack(window, 10);
-    VStack vstack(10);
-
-    Button btn1(200, 120, "click me", sf::Color::White, 2);
-    Button btn2(200, 120, "poggies!", sf::Color::White, 2);
-    Button btn3(200, 120, "poggies!", sf::Color::White, 2);
-
-    vstack.add(&btn1);
-    vstack.add(&btn2);
-
-    window.add(&vstack);
-
-    window.run();
-}
-*/
-
-///*
 int main() {
     Window window(800, 600, "Velvet");
 
-    VStack mainStack(10);
-
-    HStack s1(10);
-
-    HStack s2(10);
-
-    HStack s3(10);
-    VStack s3a(10);
-    VStack s3b(10);
-
-    Button b1(100, 100, "EGG");
-    Button b2(100, 100, "EGG");
-    Button b3(100, 100, "EGG");
-
-    Button b4(100, 300, "EGG");
-    Button b5(100, 300, "EGG");
-    Button b6(100, 300, "EGG");
-
-    Button b7(150, 100, "EGG");
-    Button b8(150, 100, "EGG");
-
-    Button b9(150, 100, "EGG");
-    Button b10(150, 100, "EGG");
-
-    s1.add(&b1);
-    s1.add(&b2);
-    s1.add(&b3);
-
-    s2.add(&b4);
-    s2.add(&b5);
-    s2.add(&b6);
-
-    s3a.add(&b7);
-    s3a.add(&b8);
+    VStack root(10);
     
-    s3b.add(&b9);
-    s3b.add(&b10);
+    Button btn(100, 50,"hello");
+    Button btn2(100, 50,"hello 2");
 
-    mainStack.add(&s1);
-    mainStack.add(&s2);
-    mainStack.add(&s3);
+    root.add(&btn);
+    root.add(&btn2);
 
-    s3.add(&s3a);
-    s3.add(&s3b);
-    
-    window.add(&mainStack);
-
+    window.add(&root);
     window.run();
 }
-//*/
